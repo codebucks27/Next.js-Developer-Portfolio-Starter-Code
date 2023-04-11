ba1a6fb534b4d892cd5916f946e0d1ac500cbcb1
ggoulartec <gustavo.49472-2021@aluno.unicv.edu.br>
2023-04-11 15:52:34 -0300

    updates
    icons na navbar e layout para home page.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,14 +2,20 @@ import Link from "next/link";
 import React from "react";
 import Logo from "./Logo";
 import { useRouter } from "next/router";
+import {TwitterIcon, GithubIcon, LinkedInIcon, PinterestIcon, DribbbleIcon } from "./Icons";
+import { motion } from "framer-motion";
 
-const CustomLink = ({href, title, className=""}) => {
+const CustomLink = ({ href, title, className = "" }) => {
   const router = useRouter();
-  console.log(router)
   return (
     <Link href={href} className={`${className} relative group`}>
       {title}
-      <span className='h-[1px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300'>&nbsp;</span>
+      <span className={`
+      h-[1px] inline-block w-0 bg-dark
+      absolute left-0 -bottom-0.5 group-hover:w-full
+      transition-[width] ease duration-300
+      ${router.asPath === href ? 'w-full' : 'w-0'}
+      `}>&nbsp;</span>
     </Link>
   )
 }
@@ -18,18 +24,37 @@ const NavBar = () => {
   return (
     <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
       <nav>
-        <CustomLink href="/" title="Home" className="mr-4"/>
-        <CustomLink href="/about" title="About" className="ml-4"/>
-        <CustomLink href="/projects" title="Projects" className="mx-4"/>
-        <CustomLink href="/articles" title="Articles" className="ml-4"/>
+        <CustomLink href="/"         title="Home"     className="mr-4" />
+        <CustomLink href="/about"    title="About"    className="mx-4" />
+        <CustomLink href="/projects" title="Projects" className="mx-4" />
+        <CustomLink href="/articles" title="Articles" className="ml-4" />
       </nav>
-      <nav>
-        <Link href="/" target={"_blank"}>T</Link>
-        <Link href="/" target={"_blank"}>T</Link>
-        <Link href="/" target={"_blank"}>T</Link>
-        <Link href="/" target={"_blank"}>T</Link>
-        <Link href="/" target={"_blank"}>T</Link>
-        <Link href="/" target={"_blank"}>T</Link>
+      <nav className="flex items-center justify-center flex-wrap">
+        <motion.a href="https://twitter.com" target={"_blank"}
+        whileHover={{y:-2}}
+        whileTap={{scale:0.9}}
+        className="w-6 mr-3"
+        ><TwitterIcon/></motion.a>
+        <motion.a href="https://twitter.com" target={"_blank"}
+        whileHover={{y:-2}}
+        whileTap={{scale:0.9}}
+        className="w-6 mr-3"
+        ><GithubIcon/></motion.a>
+        <motion.a href="https://twitter.com" target={"_blank"}
+        whileHover={{y:-2}}
+        whileTap={{scale:0.9}}
+        className="w-6 mr-3"
+        ><LinkedInIcon/></motion.a>
+        <motion.a href="https://twitter.com" target={"_blank"}
+        whileHover={{y:-2}}
+        whileTap={{scale:0.9}}
+        className="w-6 mr-3"
+        ><PinterestIcon/></motion.a>
+        <motion.a href="https://twitter.com" target={"_blank"}
+        whileHover={{y:-2}}
+        whileTap={{scale:0.9}}
+        className="w-6 mr-3"
+        ><DribbbleIcon/></motion.a>
       </nav>
       <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
         <Logo />
