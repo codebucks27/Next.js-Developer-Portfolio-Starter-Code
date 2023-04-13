282dda1e11160a748caf1c3ae7e67dfe41d4d954
ggoulartec <gustavo.49472-2021@aluno.unicv.edu.br>
2023-04-13 08:31:56 -0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Layout from './Layout'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg'>
+      <Layout className='py-8 flex items-center justify-between'>
+        <span>{new Date().getFullYear()} &copy; All Rights Reserved</span>
+        <div className='flex items-center'>
+          Build With <span className='text-primary text-2xl px-1'>&#9825;</span>
+          by&nbsp;
+          <Link href="https://devdreaming.com" className='underline underline-offset-2'>T-Wrecks</Link>
+        </div>
+        <Link href="/">Say hello</Link>
+      </Layout>
+    </footer>
   )
 }
 
