dbb596170b9bd33c9436bb4f852dea49617c8d51
ggoulartec <gustavo.49472-2021@aluno.unicv.edu.br>
2023-04-13 17:33:43 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import profilePic from "../../public/images/profile/developer-pic-1.png"
 import AnimatedText from "@/components/AnimatedText";
 import Link from "next/link";
 import { LinkArrow } from "@/components/Icons";
+import HireMe from "@/components/HireMe";
+import lightBulb from '../../public/images/svgs/miscellaneous_icons_1.svg';
 
 export default function Home() {
   return (
@@ -20,20 +22,24 @@ export default function Home() {
               <Image src={profilePic} alt="GustavoGoularte" className='w-full h-auto' />
             </div>
             <div className='w-1/2 flex flex-col items-center self-center'>
-              <AnimatedText text="Turning Vision Into Reality With Code and Design." className='!text-6xl' />
+              <AnimatedText text="Turning Vision Into Reality With Code and Design." className='!text-6xl !text-left' />
               <p className='my-4 text-base font-medium'>
                 As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
                 Explore my latest projects and articles, showcasing my expertise in React.js and web development.
               </p>
               <div className='flex items-center self-start mt-2'>
                 <Link href="/Responsive CV using Html & CSS.pdf" target={"_blank"} className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark" download={true}>Resume
-                <LinkArrow className={"w-6 ml-1"}/>
+                  <LinkArrow className={"w-6 ml-1"} />
                 </Link>
                 <Link href="mailto:ggoulartec@gmail.com" className="ml-4 text-lg font-medium capitalize text-dark underline">Contact</Link>
               </div>
             </div>
           </div>
         </Layout>
+        <HireMe />
+        <div className="absolute right-8 bottom-8 inline-block w-24">
+          <Image src={lightBulb} alt="T-Wrecks" className="w-full h-auto" />
+        </div>
       </main>
     </>
   )
