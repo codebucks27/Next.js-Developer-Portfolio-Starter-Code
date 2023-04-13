833507cd1734a37ea73dca4670ada64d019f3ccd
ggoulartec <gustavo.49472-2021@aluno.unicv.edu.br>
2023-04-13 17:33:39 -0300

    Create HireMe.js

---
+++ src/components/HireMe.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { CircularText } from './Icons'
+import Link from 'next/link'
+
+const HireMe = () => {
+  return (
+    <div className='fixed left-4 bottom-4 flex items-center justify-center overflow-hidden'>
+      <div className='w-48 h-auto flex items-center justify-center relative'>
+        <CircularText className={"fill-dark animate-spin-slow"} />
+        <Link href="mailto:ggoulartec@gmail.com" className='flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light shadow-md border border-solid border-dark w-20 h-20 rounded-full font-semibold hover:bg-light hover:text-dark'>Hire Me</Link>
+      </div>
+    </div>
+  )
+}
+
+export default HireMe
