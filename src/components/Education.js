fd66da5082534a12499da809dfeeb5c4fa51b2b8
Brian Williams <85541988+bwilliams91@users.noreply.github.com>
2023-05-09 21:06:28 -0600

    Projects page

--- src/components/Education.js
+++ src/components/Education.js
@@ -34,9 +34,7 @@ const Education = () => {
   });
   return (
     <div className="my-64">
-      <h2 className="font-bold text-8xl mb-32 w-full text-center">
-        Experience
-      </h2>
+      <h2 className="font-bold text-8xl mb-32 w-full text-center">Education</h2>
 
       <div ref={ref} className="w-[75%] mx-auto relative">
         <motion.div
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,6 +2,47 @@ import React from "react";
 import Head from "next/head";
 import Layout from "@/components/layout";
 import AnimatedText from "@/components/AnimatedText";
+import Link from "next/link";
+import Image from "next/image";
+import { GithubIcon } from "@/components/Icons";
+
+const FeaturedProject = ({ type, title, summary, img, link, github }) => {
+  return (
+    <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12">
+      <Link
+        href={link}
+        target="_blank"
+        className="w-1/2 cursor-pointer overflow-hideen rounded-lg"
+      >
+        <Image src={img} alt={title} className="w-full h-auto" />
+      </Link>
+
+      <div className="w-1/2 flex flex-col items-start justify-between pl-6">
+        <span className="text-primary font-medium text-xl">{type}</span>
+        <Link
+          href={link}
+          target="_blank"
+          className="hover:underline underline-offset-2"
+        >
+          <h2 className="my-2 w-full text-left text-4xl font-bold">{title}</h2>
+        </Link>
+        <p className="my-2 font-medium text-dark ">{summary}</p>
+        <div className="mt-2 flex items-center">
+          <Link href={github} target="_blank" className="w-10">
+            <GithubIcon />
+          </Link>
+          <Link
+            href={link}
+            target="_blank"
+            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold"
+          >
+            Visit Project
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+};
 
 const projects = () => {
   return (
@@ -10,9 +51,27 @@ const projects = () => {
         <title>Brian Williams | Projects Page</title>
         <meta name="description" content="any description" />
       </Head>
-      <main>
-        <Layout>
-          <AnimatedText text="Innovation Meets Usability!" />
+      <main className="w-full mb-16 flex flex-col items-center justify-center">
+        <Layout className="pt-16">
+          <AnimatedText text="Innovation Meets Usability!" className="mb-16" />
+
+          <div className="grid grid-cols-12 gap-24">
+            <div className="col-span-12">
+              <FeaturedProject
+                title="Star Shower"
+                img={""}
+                summary="A randomly generated star shower effect with collision."
+                link="/"
+                type="Featured Project"
+                github="/"
+              />
+            </div>
+            <div className="col-span-6">Project-1</div>
+            <div className="col-span-6">Project-2</div>
+            <div className="col-span-12">Featured Projects</div>
+            <div className="col-span-6">Project-3</div>
+            <div className="col-span-6">Project-4</div>
+          </div>
         </Layout>
       </main>
     </>
