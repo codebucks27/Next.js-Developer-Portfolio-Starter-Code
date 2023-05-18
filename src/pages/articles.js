e85b8a3cfe851252224ed065ed6bfb7a13ffa8d5
Brian Williams <85541988+bwilliams91@users.noreply.github.com>
2023-05-18 10:37:17 -0600

    Finished the project page

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,7 +1,29 @@
+import AnimatedText from "@/components/AnimatedText";
+import Layout from "@/components/layout";
 import React from "react";
+import Head from "next/head";
 
 const articles = () => {
-  return <div>articles</div>;
+  return (
+    <>
+      <Head>
+        <title>Brian Williams | Articles Page</title>
+        <meta name="description" content="any description" />
+      </Head>
+      <main className="w-full mb-16 flex flex-col items-center justify-center overflow-hidden">
+        <Layout className="pt-16">
+          <AnimatedText
+            text="I Don't Know, Satan Is Pretty Cool"
+            className="mb-16"
+          />
+          <ul className="grid grid-col-2 gap-16">
+            <li>Featured Article</li>
+            <li>Featured Article</li>
+          </ul>
+        </Layout>
+      </main>
+    </>
+  );
 };
 
 export default articles;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,14 +6,16 @@ import Link from "next/link";
 import Image from "next/image";
 import { GithubIcon } from "@/components/Icons";
 import StarShower from "../../public/images/projects/Brians_Stuff-Star_Shower.webp";
+import Omnifood from "../../public/images/projects/Brians_Stuff-Omnifood_Project.webp";
 
 const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   return (
-    <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12">
+    <article className="w-full flex items-center justify-between relative rounded-3xl rounded-br-2xl border border-solid border-dark bg-light shadow-2xl p-12">
+      <div className="absolute top-0 -right-3 -z-10 w-[100%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl" />
       <Link
         href={link}
         target="_blank"
-        className="w-1/2 cursor-pointer overflow-hideen rounded-lg"
+        className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
       >
         <Image src={img} alt={title} className="w-full h-auto rounded-xl" />
       </Link>
@@ -45,6 +47,45 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   );
 };
 
+const Project = ({ title, type, img, link, github }) => {
+  return (
+    <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative">
+      <div className="absolute top-0 -right-3 -z-10 w-[100%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl" />
+      <Link
+        href={link}
+        target="_blank"
+        className="w-full cursor-pointer overflow-hidden rounded-lg"
+      >
+        <Image src={img} alt={title} className="w-full h-auto rounded-xl" />
+      </Link>
+
+      <div className="w-full flex flex-col items-start justify-between mt-4">
+        <span className="text-primary font-medium text-xl">{type}</span>
+        <Link
+          href={link}
+          target="_blank"
+          className="hover:underline underline-offset-2"
+        >
+          <h2 className="my-2 w-full text-left text-3xl font-bold">{title}</h2>
+        </Link>
+
+        <div className="w-full mt-2 flex items-center justify-between">
+          <Link
+            href={link}
+            target="_blank"
+            className="ml-4 text-lg font-semibold"
+          >
+            Visit
+          </Link>
+          <Link href={github} target="_blank" className="w-8">
+            <GithubIcon />
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+};
+
 const projects = () => {
   return (
     <>
@@ -56,7 +97,7 @@ const projects = () => {
         <Layout className="pt-16">
           <AnimatedText text="Innovation Meets Usability!" className="mb-16" />
 
-          <div className="grid grid-cols-12 gap-24">
+          <div className="grid grid-cols-12 gap-24 gap-y-32">
             <div className="col-span-12">
               <FeaturedProject
                 title="Star Shower"
@@ -67,11 +108,52 @@ const projects = () => {
                 github="https://github.com/bwilliams91/Star-Shower"
               />
             </div>
-            <div className="col-span-6">Project-1</div>
-            <div className="col-span-6">Project-2</div>
-            <div className="col-span-12">Featured Projects</div>
-            <div className="col-span-6">Project-3</div>
-            <div className="col-span-6">Project-4</div>
+            <div className="col-span-6">
+              <Project
+                title="Omnifood Food Delivery"
+                img={Omnifood}
+                link="https://starshowerbkw.netlify.app/"
+                type="Project"
+                github="https://github.com/bwilliams91/Star-Shower"
+              />
+            </div>
+            <div className="col-span-6">
+              <Project
+                title="Omnifood Food Delivery"
+                img={Omnifood}
+                link="https://starshowerbkw.netlify.app/"
+                type="Project"
+                github="https://github.com/bwilliams91/Star-Shower"
+              />
+            </div>
+            <div className="col-span-12">
+              <FeaturedProject
+                title="Star Shower"
+                img={StarShower}
+                summary="A randomly generated star shower effect with collision."
+                link="https://starshowerbkw.netlify.app/"
+                type="Featured Project"
+                github="https://github.com/bwilliams91/Star-Shower"
+              />
+            </div>
+            <div className="col-span-6">
+              <Project
+                title="Omnifood Food Delivery"
+                img={Omnifood}
+                link="https://starshowerbkw.netlify.app/"
+                type="Project"
+                github="https://github.com/bwilliams91/Star-Shower"
+              />
+            </div>
+            <div className="col-span-6">
+              <Project
+                title="Omnifood Food Delivery"
+                img={Omnifood}
+                link="https://starshowerbkw.netlify.app/"
+                type="Project"
+                github="https://github.com/bwilliams91/Star-Shower"
+              />
+            </div>
           </div>
         </Layout>
       </main>
