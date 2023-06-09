c1e5a7116e43ed212a3d7bacd6948868350ba35e
abdallah1936 <awadallaabdallah@gmail.com>
2023-06-08 21:59:17 -0700

    added the tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
