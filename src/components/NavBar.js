e1dda733264d7ddd815e18a5fdfaaf131fd1f188
abdallah1936 <awadallaabdallah@gmail.com>
2023-06-10 18:02:34 -0700

    working on NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,24 @@
 import Link from "next/link";
 import { REACT }  from "react";
+import { useRouter } from "next/router";
 import Logo from "./Logo";
+
+const CustomLink = ({ href, title, className="" }) => {
+  const router = useRouter();
+
+  
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+
+      <span className={`h-[1px] inline-block w-0 bg-black
+       absolute left-0 -bottom-0.5
+       group-hover:w-full transition-[width] ease duration-300
+       ${router.aspath === href ? "w-full" : "w-0"}
+       `}>&nbsp;</span>
+    </Link>
+  );
+}
 
 const NavBar = () => {
   return (
@@ -8,10 +26,10 @@ const NavBar = () => {
       className='w-full px-32 py-8 font-medium flex items-center justify-between'
       >
         <nav>
-          <Link href='/'>Home</Link>
-          <Link href='/about'>About</Link>
-          <Link href='/projects'>projects</Link>
-          <Link href='/articles'>articles</Link>
+          <CustomLink href='/' title="Home" className="mr-4"/>
+          <CustomLink href='/about' title="About" className="mx-4"/>
+          <CustomLink href='/projects' title="Projects"className="mx-4"/>
+          <CustomLink href='/articles' title="Articles" className="ml-4"/>
         </nav>
         
         <nav>
