fc8a8adbaa54b772c3e3c874125837925982d299
abdallah1936 <awadallaabdallah@gmail.com>
2023-06-23 18:04:40 -0700

    updated home pic

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
     className="w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light sm:text-base "
     >
       <Layout className="py-8 flex items-center justify-between lg:flex-col lg:py-6">
-        <span>{new Date().getFullYear()} &copy; All Rights Reserverd.</span>
+        <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
         <div className="flex items-center lg:py-2">
         Built With <span className="text-primary dark:text-primaryDark text-2xl px-1">&#9825;</span> by&nbsp;<Link href="http://linkedin.com/in/abdallahawadalla/" className="underline underline-offset-2">Abdallah Awadalla</Link>
         </div>
